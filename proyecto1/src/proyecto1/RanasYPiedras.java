9551fc18eee983403ef635e83b55b0d5d2f03e90
David Leon <da.leon@uniandes.edu.co>
2023-09-28 03:06:45 -0500

    implementacion con programacion dinamica
    ahora usamos una matriz tridimensional para guardar los posibles movimientos. En el doc hay un ejemplo de como debería funcionar

--- proyecto1/src/proyecto1/RanasYPiedras.java
+++ proyecto1/src/proyecto1/RanasYPiedras.java
@@ -1,12 +1,8 @@
 package proyecto1;
 
 import java.util.Scanner;
-import java.util.HashSet;
-import java.util.Set;
 
 public class RanasYPiedras {
-	
-	public static String disposicionOriginal;
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -19,59 +15,49 @@ public class RanasYPiedras {
             int r = scanner.nextInt();
             int m = scanner.nextInt();
             String disposicion = scanner.next();
-            disposicionOriginal = new String (disposicion);
 
-            Set<String> disposiciones = new HashSet<>();
-            disposiciones.add(disposicion);
+            System.out.println("Disposiciones generadas para el caso " + (caso + 1) + ":");
 
-            int resultado = contarDisposiciones(p, r, m, disposicion, disposiciones);
+            contarDisposiciones(p, r, m, disposicion);
 
-            //Imprimir las disposiciones generadas para este caso
-         System.out.println("Disposiciones generadas para el caso " + (caso + 1) + ":");
-         for (String disp : disposiciones) {
-               System.out.println(disp);
-           }
-
-            // Imprimir el número de disposiciones después de m movimientos
-            System.out.println(resultado);
+            System.out.println(); // Agregar línea en blanco entre casos
         }
 
         scanner.close();
     }
 
-    public static int contarDisposiciones(int p, int r, int m, String disposicion, Set<String> disposiciones) {
-        if (m == 0) {
-            return 1;
+    public static void contarDisposiciones(int p, int r, int m, String disposicion) {
+        int[][][] dp = new int[m + 1][p][p];
+
+        // Inicializar la matriz dp con la disposición inicial
+        for (int fila = 0; fila < p; fila++) {
+            for (int rana = 0; rana < p; rana++) {
+                dp[0][fila][rana] = (disposicion.charAt(rana) == 'r') ? 1 : 0;
+            }
         }
 
-        int resultado = 0;
-
-        for (int i = 0; i < p; i++) {
-            if (disposicion.charAt(i) == 'r') {
-                if (i > 0 && disposicion.charAt(i - 1) == 'p') {
-                    String nuevaDisposicion = disposicion.substring(0, i - 1) + "rp" + disposicion.substring(i + 1);
-                    if (disposiciones.add(nuevaDisposicion) && esMovimientoValido(disposicionOriginal, nuevaDisposicion, i - 1, i)) {
-                        resultado += contarDisposiciones(p, r, m - 1, disposicionOriginal, disposiciones);
-                    }
-                }
-                if (i < p - 1 && disposicion.charAt(i + 1) == 'p') {
-                    String nuevaDisposicion = disposicion.substring(0, i) + "pr" + disposicion.substring(i + 2);
-                    if (disposiciones.add(nuevaDisposicion) && esMovimientoValido(disposicionOriginal, nuevaDisposicion, i, i + 1)) {
-                        resultado += contarDisposiciones(p, r, m - 1, disposicionOriginal, disposiciones);
+        // Calcular dp usando programación dinámica y contar disposiciones en cada movimiento
+        for (int mov = 1; mov <= m; mov++) {
+            for (int fila = 0; fila < p; fila++) {
+                for (int rana = 0; rana < p; rana++) {
+                    for (int nuevaRana = 0; nuevaRana < p; nuevaRana++) {
+                        if (rana != nuevaRana) {
+                            dp[mov][fila][rana] += dp[mov - 1][fila][nuevaRana];
+                        }
                     }
                 }
             }
         }
-        
-        int respuesta = disposiciones.size() - 1;
 
-
-        return respuesta;
-    }
-
-    // Función para verificar si un movimiento es válido
-    public static boolean esMovimientoValido(String disposicionActual, String nuevaDisposicion, int ranaOriginal, int ranaNueva) {
-        int distancia = Math.abs(ranaNueva - ranaOriginal);
-        return distancia == 1;
+        // Imprimir la matriz en cada movimiento
+        for (int mov = 0; mov <= m; mov++) {
+            System.out.println("Matriz en el movimiento " + mov + ":");
+            for (int fila = 0; fila < p; fila++) {
+                for (int rana = 0; rana < p; rana++) {
+                    System.out.print(dp[mov][fila][rana] + " ");
+                }
+                System.out.println();
+            }
+        }
     }
 }
